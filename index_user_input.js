3c79b9675044f5fc3bd6baac7d80bed9934c3d76
gmlee21 <gm_lee21@yahoo.com.sg>
2023-11-29 08:25:10 +0800

    Edited when the "Generated Button" appears

--- index_user_input.js
+++ index_user_input.js
@@ -1,28 +1,53 @@
 function toggleAdditionalInputs() {
 
     var projectType = document.getElementById("projectType");
+    var procurementApproach = document.getElementById("procurementApproach");
+    var developmentScale = document.getElementById("developmentScale");
+    var systemCost = document.getElementById("systemCost");
+    var trialDuration = document.getElementById("trialDuration");
+
     var additionalInput1 = document.getElementById("additionalInput1");
     var additionalInput2 = document.getElementById("additionalInput2");
     var additionalInput3 = document.getElementById("additionalInput3");
+    var additionalInput4 = document.getElementById("additionalInput4"); 
 
     additionalInput1.classList.add("hidden");
     additionalInput2.classList.add("hidden");
     additionalInput3.classList.add("hidden");
+    additionalInput4.classList.add("hidden");
 
-    var selectedValue = projectType.value;
+    var selectedValueA = projectType.value;
+    var selectedValueB = procurementApproach.value;
+    var selectedValue1 = developmentScale.value;
+    var selectedValue2 = systemCost.value;
+    var selectedValue3 = trialDuration.value;
 
-    if (selectedValue.toLowerCase() === "1"){
-        additionalInput3.classList.remove("hidden");
-    } else if (selectedValue.toLowerCase() === "2") {
+    if (selectedValueA === "1") {
+            additionalInput3.classList.remove("hidden");
+        if (selectedValueB !== "0" && selectedValue3 !== "0") {
+            console.log("unhide");
+            additionalInput4.classList.remove("hidden"); 
+        }
+    }
+
+    if (selectedValueA === "2") {
         additionalInput1.classList.remove("hidden");
         additionalInput3.classList.remove("hidden");
-    } else {
+        if (selectedValueB !== "0" && selectedValue1 != "0" && selectedValue3 !== "0") {
+            additionalInput4.classList.remove("hidden");
+        }
+    } 
+    
+    if (selectedValueA === "3") {
         additionalInput2.classList.remove("hidden");
+        if (selectedValueB !== "0" && selectedValue2 !== "0" ) {
+            additionalInput4.classList.remove("hidden");
+        }
     }
   }
   
-  document.getElementById("projectType").addEventListener("change", toggleAdditionalInputs);
-
+  document.getElementById("calculator").addEventListener("change", toggleAdditionalInputs);
+  
 function goBack() {
     window.history.back();
 }
